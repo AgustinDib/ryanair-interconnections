d6ed0c0943ff7b01356da8060fa70ea70d5d2720
agustindib <agustin.dib@joinmosaic.com>
2019-08-01 19:38:38 -0300

    Controller added.

---
+++ src/main/java/com/ryanair/flights/controller/FlightController.java
@@ -0,0 +1,66 @@
+package com.ryanair.flights.controller;
+
+import com.ryanair.flights.exception.RestClientException;
+import com.ryanair.flights.exception.ValidationException;
+import com.ryanair.flights.service.FlightServiceI;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/flight")
+class FlightController {
+
+    private final FlightServiceI flightService;
+
+    @Autowired
+    public FlightController(FlightServiceI flightService) {
+        this.flightService = flightService;
+    }
+
+    /**
+     * Returns a list of flights departing from a given departure airport not earlier than the specified departure
+     * datetime and arriving to a given arrival airport not later than the specified arrival datetime.
+     * @param departure airport expressed in IATA code.
+     * @param arrival airport expressed in IATA code.
+     * @param depDate departure date in ISO.DATE_TIME format.
+     * @param arrDate arrival date in ISO.DATE_TIME format.
+     * @return a ResponseEntity with an HttpStatus and a List of FlightResponse for successful hits, or a body
+     * explaining the problem for failures.
+     */
+    @GetMapping("/interconnections")
+    ResponseEntity<?> interconnections(
+        @NotNull @RequestParam("departure") String departure,
+        @NotNull @RequestParam("arrival") String arrival,
+        @NotNull @RequestParam("departureDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime depDate,
+        @NotNull @RequestParam("arrivalDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime arrDate
+    ) {
+        String logHeader = "FlightController.interconnections: ";
+        log.info(logHeader + "request received for departure: " + departure + ", arrival: " + arrival +
+                ", departureDateTime: " + depDate + ", arrivalDateTime: " + arrDate);
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(flightService.findInterconnections(departure, arrival, depDate, arrDate));
+        } catch (ValidationException e) {
+            log.error(logHeader + e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (RestClientException e) {
+            log.error(logHeader + e.getMessage(), e);
+            return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+        } catch (Exception e) {
+            log.error(logHeader + e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+}
